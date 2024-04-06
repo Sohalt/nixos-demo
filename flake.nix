018913352ad244510f9a4b3c065a82f76a17ce4b
sohalt <sohalt@sohalt.net>
2024-04-06 23:39:24 +0200

    Only use hello from old nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,20 +1,22 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/942b0817e898262cc6e3f0a5f706ce09d8f749f1";
+    nixpkgs-hello.url = "github:nixos/nixpkgs/942b0817e898262cc6e3f0a5f706ce09d8f749f1";
     utils.url = "github:numtide/flake-utils";
   };
   outputs = {
     self,
     nixpkgs,
+    nixpkgs-hello,
     utils,
   }:
     utils.lib.eachDefaultSystem (
       system: let
         pkgs = nixpkgs.legacyPackages.${system};
+        hello-pkgs = nixpkgs-hello.legacyPackages.${system};
       in {
         devShell = pkgs.mkShell {
           buildInputs = with pkgs; [
-            (hello.overrideAttrs (oldAttrs: {
+            (hello-pkgs.hello.overrideAttrs (oldAttrs: {
               patches = [./hello.patch];
               doCheck = false;
             }))
